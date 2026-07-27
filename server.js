5e6158858209f1b536eed8ae631eb638e3a41cac
Raz Rizal <razrizal20@gmail.com>
2026-07-27 23:26:52 +0800

    Fix Telegram bot import

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
 
-const TelegramBot = require("node-telegram-bot-api");
+const TelegramBot = require("node-telegram-bot-api").default;
 
 const {
     addonBuilder,
